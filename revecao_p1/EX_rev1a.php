b4327595ccf81886752d2800f01224df405bbc97
Milena Salles <111434242+Sweetnemesis304@users.noreply.github.com>
2024-04-21 23:37:37 -0300

    Add files via upload

---
+++ revecao_p1/EX_rev1a.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$var = ["ADS", "CD","ADS","ADS","CD","CD","ADS"];
+$i = 0 ; 
+   while($i <7) {
+   echo $var[$i] . "\n";
+   $i++;
+   };
+
+?>
+    
+  
+?>
+    
+</body>
+</html>
+
+
+
